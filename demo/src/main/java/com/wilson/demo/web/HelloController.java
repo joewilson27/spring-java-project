f406134004fda4e313b4fd84387e141de176286a
Joe Wilson <wilsonmangunsong@gmail.com>
2022-04-15 12:45:58 +0700

    sec1_vid7
    - Create Form, collect data from input form
    - get id from url with annotation

--- demo/src/main/java/com/wilson/demo/web/HelloController.java
+++ demo/src/main/java/com/wilson/demo/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.wilson.demo.web;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -19,4 +21,35 @@ public class HelloController {
 	public String sayHello() {
 		return "<h1>hello</h1>";
 	}
+	
+	@RequestMapping("/proper") // basic mapping selalu GET, tanpa kita harus mendefine GET
+	public String sayProperHello() {
+		return "<h1>Hello there, how are you?</h1>";
+	}
+	
+	@RequestMapping("/user_entry")
+	public String userForm() {
+		return "<form action=\"/greeting/user_greeting\" method=\"POST\">\r\n"
+				+ "  First name:<br>\r\n"
+				+ "  <input type=\"text\" name=\"firstname\"><br>\r\n"
+				+ "  Last name:<br>\r\n"
+				+ "  <input type=\"text\" name=\"lastname\"><br><br>\r\n"
+				+ "  <br><br>\r\n"
+				+ "  <input type=\"submit\" value=\"Submit\">\r\n"
+				+ "</form> ";
+	}
+	
+	@RequestMapping(value="/user_greeting", method=RequestMethod.POST)
+	public String printUserGreeting(@RequestParam String firstname, @RequestParam String lastname) {
+		return "Hello there, " + firstname + " " + lastname;
+	}
+	// anotation @RequestParam --> utk param name di body form
+	// anotation @PathVariable --> utk param name di body form
+	
+	@RequestMapping(value="/orders/{id}", method = RequestMethod.GET)
+	public String getOrder(@PathVariable String id) {
+		return "Order OD: " + id;
+	}
+	
+	
 }
