ae363bf5daf24e56f78fc1810f832865a7c21512
Joe Wilson <wilsonmangunsong@gmail.com>
2022-04-15 17:47:01 +0700

    sec1_vid11
    Using model to save data form and show in the other page using thymeleaf.
    
    add Thymeleaf for view. you can add them with right click on pom.xml -> Spring -> added Starters / edit starters.
    after added, check your pom.xml file
    * add Thymeleaf from Eclipse Market because on STS4 the web option is not appear on Select Wizard (source: https://stackoverflow.com/questions/66775096/html-file-not-showing-in-select-wizard-in-spring-tool-suite)
    
    spring.thymeleaf.cache=false on application.properties in src/main/resources/templates
    set to true in production agar ter-cache
    
    pada form method index, object model yg kita defined pada
    th:object="${formData}" harus diikuti oleh input pada form tersebut
    th:field="${formData.fullName}", fullName harus sama dgn property pada object yg digunakan
    
    @RequestParam bisa digunakan utk nangkep querystring (sec1 vid9 09:37) juga pada form dgn class @RestController (sec1 vid7 14:10)

---
+++ demo/src/main/java/com/wilson/demo/web/HomeController.java
@@ -0,0 +1,29 @@
+package com.wilson.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.wilson.demo.domain.User;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home(Model model) { // of you wanna retrieve or send data interact with UI, so use model
+		model.addAttribute("formData", new User());
+		// the key "formData" harus sama dgn th:object="${formData}" pada form di index.html
+		
+		return "index"; // this would be find an index page, because on this controller class
+		// we use @Controller not RestController like the class we had earlier so then they're not return back
+		// a string. And they would find the file index on src/main/resources/template
+	}
+	
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String processFormData(User user) {
+		// object user akan di tangkap di page result.html
+		return "result";
+	}
+	
+}
